3de28264915c2e4d3ebb8ba1bcde9e7ad7332edf
Blu` <87508144+Thanasak-s63@users.noreply.github.com>
2022-03-16 20:57:04 +0700

    Add files via upload

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-    baseURL: "https://oxonline.herokuapp.com/api",
+    baseURL: "https://xoonlinebackend.herokuapp.com/api",
     headers: {
         "Content-type": "application/json",
         
