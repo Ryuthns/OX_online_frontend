ed19624d2fe866556fe17b27d68ac03e11f2893c
Blu` <87508144+Thanasak-s63@users.noreply.github.com>
2022-03-16 20:49:07 +0700

    Delete frontend directory

--- frontend/src/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-export default axios.create({
-    baseURL: "https://oxonline.herokuapp.com/api",
-    headers: {
-        "Content-type": "application/json",
-        
-    },
-});
